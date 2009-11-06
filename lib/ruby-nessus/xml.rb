3fe13e31b0c5905a93d3044c486fb5927e03c627
Dustin Willis Webber <dustin.webber@gmail.com>
2009-11-06 05:09:14 -0600

    test push

--- lib/ruby-nessus/xml.rb
+++ lib/ruby-nessus/xml.rb
@@ -10,22 +10,6 @@ module Nessus
       block.call(self) if block
     end
     
-    def node_count
-      @node_count = 0
-      @xml.each { |node| @node_count += 1 }
-      @node_count
-    end
-    
-    def list_nodes
-      @xml.each do |node|
-        puts node.name
-      end
-    end
-    
-    def hosts
-      
-    end
-    
   end
   
 end
