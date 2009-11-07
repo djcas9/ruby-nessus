82817a46bf3d85a8e9c6506278993a75d54606cc
Dustin Willis Webber <dustin.webber@gmail.com>
2009-11-06 18:57:20 -0600

    doc fixups

--- lib/ruby-nessus.rb
+++ lib/ruby-nessus.rb
@@ -1,7 +1,10 @@
+# Includes
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+# Dependencies
 require 'rubygems'
 require 'nokogiri'
 require 'chronic'
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+# Ruby-Nessus Files
 require 'ruby-nessus/nessus'
 require 'ruby-nessus/version'
