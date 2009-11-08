f8c3909fee8327df45cfaa8b10277dc2667d4ab9
Dustin Willis Webber <dustin.webber@gmail.com>
2009-11-08 16:52:30 -0600

    remove version.rb

--- lib/ruby-nessus.rb
+++ lib/ruby-nessus.rb
@@ -1,3 +1,2 @@
 # Ruby-Nessus Files
 require 'ruby-nessus/nessus'
-require 'ruby-nessus/version'
--- lib/ruby-nessus/version.rb
+++
@@ -1,5 +0,0 @@
-module Nessus
-  # Ruby-Nessus
-  # Copyright (c) 2009 Dustin Willis Webber
-  VERSION = '0.1.0'
-end
