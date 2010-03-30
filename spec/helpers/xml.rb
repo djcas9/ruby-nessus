95bb47b46b6f750cb620c085dc5186faf56d0e52
Dustin Willis Webber <dustin.webber@gmail.com>
2010-03-29 22:34:49 -0400

    fix tests for XML class change

--- spec/helpers/xml.rb
+++ spec/helpers/xml.rb
@@ -1,4 +1,6 @@
 module Helpers
-  DOT_NESSUS_V1 = File.join(File.dirname(__FILE__),'example_v1.nessus')
-  DOT_NESSUS_V2 = File.join(File.dirname(__FILE__),'example_v2.nessus')
+  v1 = File.join(File.dirname(__FILE__),'example_v1.nessus')
+  v2 = File.join(File.dirname(__FILE__),'example_v2.nessus')
+  DOT_NESSUS_V1 = Nokogiri::XML.parse(File.open(v1).read)
+  DOT_NESSUS_V2 = Nokogiri::XML.parse(File.open(v2).read)
 end
